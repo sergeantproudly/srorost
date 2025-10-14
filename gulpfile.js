9b0540e529a7e15a678b1b83b468ab055d9e5500
lianet <jack.tolmak@yandex.ru>
2025-10-14 12:46:07 +0300

    add update gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
-'use strict';
+import gulp from 'gulp';
+import { deleteAsync } from 'del';
+import browserSync from 'browser-sync';
+import concat from 'gulp-concat';
+import autoprefixer from 'gulp-autoprefixer';
+import cleanCSS from 'gulp-clean-css';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import uglify from 'gulp-uglify';
+import imagemin from 'gulp-imagemin';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminOptipng from 'imagemin-optipng';
 
-const gulp = require('gulp');
-const del = require('del');
-const browserSync = require('browser-sync').create();
-
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');   
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
+const sass = gulpSass(dartSass);
 
 const jsFiles = [
 	'./src/js/jquery-3.1.1.min.js',
@@ -19,79 +21,72 @@ const jsFiles = [
 	'./src/js/jquery.touchSwipe.min.js',
 	'./src/js/jquery.mousewheel.min.js',
 	'./src/js/jquery.magnific-popup.js',
-	'./src/js/slick.min.js',	
+	'./src/js/slick.min.js',
 	'./src/js/wow.min.js',
 	'./src/js/checks.js',
 	'./src/js/messages.js',
 	'./src/js/scripts.js'
 ];
 
-sass.compiler = require('node-sass');
-
 function styles() {
 	return gulp.src('./src/css/style.scss')
-				.pipe(sourcemaps.init())
-				.pipe(concat('style.css'))		
-				.pipe(autoprefixer({
-		            browsers: ['> 0.1%'],
-		            cascade: false
-		        }))
-		        /*
-		        .pipe(cleanCSS({
-		        	level: 2
-		        }))
-		        */
-		        .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
-		        //.pipe(sourcemaps.write())
-				.pipe(gulp.dest('./assets/css'))
-				.pipe(browserSync.stream());
-}
+		.pipe(sourcemaps.init())
+		.pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+		.pipe(autoprefixer({ overrideBrowserslist: ['> 0.1%'], cascade: false }))
+		.pipe(cleanCSS({ level: 2 }))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./assets/css'))
+		.pipe(browserSync.stream());
+};
 
 function scripts() {
 	return gulp.src(jsFiles)
-				.pipe(concat('scripts.js'))
-				.pipe(uglify({
-					toplevel: true
-				}))
-				.pipe(gulp.dest('./assets/js'))
-				.pipe(browserSync.stream());
-}
+		.pipe(concat('scripts.js'))
+		.pipe(uglify({
+			toplevel: true
+		}))
+		.pipe(gulp.dest('./assets/js'))
+		.pipe(browserSync.stream());
+};
 
 function images() {
-	return gulp.src('./src/images/*')
-        .pipe(imagemin({
-        	progressive: true
-        }))
-        .pipe(gulp.dest('./assets/images'));
-}
+	return gulp.src('./src/images/**/*')
+		.pipe(imagemin([
+			imageminMozjpeg({ quality: 75, progressive: true }),
+			imageminOptipng({ optimizationLevel: 5 })
+		]))
+		.pipe(gulp.dest('./assets/images'));
+};
 
 function fonts() {
 	return gulp.src('./src/fonts/**/*')
 		.pipe(gulp.dest('./assets/fonts'));
-}
+};
 
-function watch() {
+function clean() {
+	return deleteAsync(['assets/*']);
+};
+
+function watchFiles() {
 	browserSync.init({
-        proxy: 'srorost.local'
-    });
+		proxy: 'srorost.local',
+		notify: false,
+		open: false
+	});
 
 	gulp.watch('./src/css/**/*.scss', styles);
 	gulp.watch('./src/js/**/*.js', scripts);
-	gulp.watch('./**/*.htm').on('change', browserSync.reload);
-	gulp.watch('./**/*.php').on('change', browserSync.reload);
-}
+	gulp.watch(['./**/*.htm', './**/*.php']).on('change', browserSync.reload);
+};
 
-function clean() {
-	return del(['assets/*']);
-}
+const build = gulp.series(clean,
+	gulp.parallel(styles, scripts, images, fonts)
+);
 
-//gulp.task('styles', styles);
-//gulp.task('scripts', scripts);
-gulp.task('images', images);
-gulp.task('watch', watch);
+const dev = gulp.series(build, watchFiles);
 
-gulp.task('build', gulp.series(clean,
-						gulp.parallel(styles, scripts, images, fonts)
-					));
 
-gulp.task('dev', gulp.series('build', 'watch'));
+export { styles, scripts, images, fonts, clean, watchFiles, build, dev };
+
+
+export default dev;
